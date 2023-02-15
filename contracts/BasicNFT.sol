be02688ea6b1a5911ddcb7e2fb127263c553877b
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-15 16:43:48 +0000

    start with BasicNft contract

--- contracts/BasicNFT.sol
+++ contracts/BasicNFT.sol
@@ -1,7 +1,27 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT {
-    
+pragma solidity ^0.8.7;
+
+contract BasicNFT is ERC721{
+    //
+
+    uint256 private s_tokenCounter;
+    String public constant TOKEN_URI = "http://bafybeibrgyezr5muh6qekxsnt4st5ljzz4ed5slivop2vzivgsl2y3ocim.ipfs.localhost:8080/";
+
+    constructor() ERC721 ("KAMITA", "KHMT") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns(uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns(uint256) {
+        return s_tokenCounter
+    }
+
 }
