5ff1bd653059ac46fce613fdebb3db3dc24a4486
sylvain tagnabou <sylvaintagnabou@gmail.com>
2023-02-16 12:05:40 +0000

    (done): bascNft test

--- deploy/01-deploy-basicNFT.js
+++ deploy/01-deploy-basicNFT.js
@@ -1,3 +1,30 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper.config")
 
 
-!
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+
+    log("_________________Deploying BasicNFT_________________")
+    const args = []
+    const basicNft = await deploy("BasicNFT", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, arguments)
+    }
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, arguments)
+    }
+}
+
+module.exports.tags = ['all', 'basicnft', 'main']
